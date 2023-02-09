2ca7008ae47092bd1801fbc551a805d6e2b305bf
Doge <dogeconectbsctoken@gmail.com>
2023-02-09 18:18:55 +0700

    init
    init

---
+++ assets/loading-f780bf05.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-28f61451.js";const s=o(!1),r=()=>s;export{r as u};
---
+++ assets/store-31126b24.js
@@ -0,0 +1,1 @@
+import{c as t}from"./pinia-81e480ea.js";import"./index-28f61451.js";const a={priority:-100,install({use:r}){r(t())}};export{a as store};
